b58ab17fdebf12245417f08797fd45dfac67ef16
tomsawada <tomydrum@gmail.com>
2021-09-10 20:45:39 -0400

    Conditional navigation. Smoother for Grid and very acceptable for all other cases

--- acpic/lib/main.dart
+++ acpic/lib/main.dart
@@ -6,6 +6,7 @@ import 'dart:io' show Platform;
 import 'package:acpic/screens/grid.dart';
 import 'package:acpic/screens/photo_access_needed.dart';
 import 'package:acpic/screens/login_screen.dart';
+import 'package:acpic/screens/distributor.dart';
 // IMPORT UI ELEMENTS
 import 'package:acpic/ui_elements/constants.dart';
 //IMPORT SERVICES
@@ -46,98 +47,25 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    print('loggedInLocal in MyApp Build is $loggedInLocal');
-    print('permissionLevel in MyApp Build is $permissionLevel');
     return MaterialApp(
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Distributor(),
-      // permissionLevel != 'granted' || loggedInLocal == false
-      //     ? Distributor()
-      //     : GridPage(),
+      home: loggedInLocal == true && permissionLevel == 'granted'
+          ? GridPage()
+          // : loggedInLocal == false && permissionLevel == 'granted' ||
+          //         permissionLevel == 'denied' ||
+          //         permissionLevel == 'limited' ||
+          //         permissionLevel == 'permanent' ||
+          //         permissionLevel == 'restricted'
+          //     ? LoginScreen()
+          : Distributor(),
       routes: {
         GridPage.id: (context) => GridPage(),
         LoginScreen.id: (context) => LoginScreen(),
         PhotoAccessNeeded.id: (context) => PhotoAccessNeeded(),
         RequestPermission.id: (context) => RequestPermission(),
       },
-    );
-  }
-}
-
-class Distributor extends StatefulWidget {
-  static const String id = 'distributor';
-
-  @override
-  _DistributorState createState() => _DistributorState();
-}
-
-class _DistributorState extends State<Distributor> {
-  bool recurringUserLocal = false;
-  bool loggedInLocal = false;
-  Future myFuture;
-  Future myFutureLoggedIn;
-
-  @override
-  void initState() {
-    if (Platform.isAndroid == true) {
-      myFuture = SharedPreferencesService.instance
-          .getBooleanValue('recurringUser')
-          .then((value) {
-        setState(() {
-          recurringUserLocal = value;
-        });
-      });
-    }
-    // TODO: Delete this function later. This is just to make the interface work as it should
-    myFutureLoggedIn = SharedPreferencesService.instance
-        .getBooleanValue('loggedIn')
-        .then((value) {
-      setState(() {
-        loggedInLocal = value;
-      });
-      return loggedInLocal;
-    });
-
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    print('loggedInLocal in Distributor Build is $loggedInLocal');
-    print('recurringUserLocal in Distributor Build is $recurringUserLocal');
-    // Conditional Navigation
-    checkPermission(context).then((value) {
-      if (loggedInLocal == false) {
-        Navigator.pushReplacementNamed(
-          context,
-          LoginScreen.id,
-          arguments: PermissionLevelFlag(permissionLevel: value),
-        );
-      } else if ((Platform.isIOS
-          ? (value == 'denied' && loggedInLocal == true)
-          : (value == 'denied' &&
-                  loggedInLocal == true &&
-                  recurringUserLocal == false ||
-              recurringUserLocal == null))) {
-        Navigator.pushReplacementNamed(context, RequestPermission.id);
-      } else if (value == 'granted' && loggedInLocal == true) {
-        Navigator.of(context).push(
-          MaterialPageRoute(builder: (_) => GridPage()),
-        );
-      } else {
-        // print('I am in Distributor else and value is $value');
-        Navigator.pushReplacementNamed(context, PhotoAccessNeeded.id,
-            arguments: PermissionLevelFlag(permissionLevel: value));
-      }
-    });
-    return Container(
-      color: Colors.white,
-      child: Center(
-          child: CircularProgressIndicator(
-        valueColor: AlwaysStoppedAnimation<Color>(kAltoBlue),
-      )),
     );
   }
 }
