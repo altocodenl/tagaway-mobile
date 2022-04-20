5cca3acb7335dcddef7a94a0612f6e1758408b4e
tomsawada <tomydrum@gmail.com>
2022-04-20 18:14:35 -0400

    Multi PIV upload trials WIP

--- acpic/ios/Runner/AppDelegate.swift
+++ acpic/ios/Runner/AppDelegate.swift
@@ -19,19 +19,23 @@ import SystemConfiguration
       var sURL: String!
       sURL = "https://altocode.nl/dev/pic/app/piv"
       var multipartDataFormResponse: String! = "A string"
-      var pHAssetArray: [PHAsset] = []
+      var imageFinal: UIImage?
       
-//      func idToPhAsset(idList: [String]){
+//      func idToPhAsset(idList: [String], cookie: String, id: Int, csrf: String, tag: String){
+//
 //      var copyOfidList: [String] = idList
-//          func populatePhAssetArray(){
+//          func populatePhAssetArray()-> Void{
 //              if(copyOfidList.isEmpty){
-//                  print(pHAssetArray)
+//                  print("Done uploading")
+//                  return
 //              }
 //              else{
-//              var phAssetPivFetchedAsset = PHAsset.fetchAssets(withLocalIdentifiers: [copyOfidList[0]], options: photosOptions)
-//              let pivPHAsset = phAssetPivFetchedAsset.firstObject! as PHAsset
-//              pHAssetArray.append(pivPHAsset)
+//                  let phAssetPivFetchedAsset = PHAsset.fetchAssets(withLocalIdentifiers: [copyOfidList[0]], options: photosOptions)
+//                  let pivPHAsset = phAssetPivFetchedAsset.firstObject! as PHAsset
+//                  let lastModified = String(Int(pivPHAsset.creationDate!.timeIntervalSince1970*1000))
 //              copyOfidList.remove(at: 0)}
+//              print(copyOfidList.count)
+//              populatePhAssetArray()
 //          }
 //          populatePhAssetArray()
 //      }
@@ -46,7 +50,7 @@ import SystemConfiguration
               let id: Int = arguments[2] as! Int
               let csrf: String = arguments[3] as! String
               let tag: String = arguments[4] as! String
-//              idToPhAsset(idList: idList)
+//              idToPhAsset(idList: idList, cookie: cookie, id: id, csrf: csrf, tag: tag)
               
               let phAssetPivFetchedAsset = PHAsset.fetchAssets(withLocalIdentifiers: [idList[0]], options: photosOptions)
               let pivPHAsset = phAssetPivFetchedAsset.firstObject! as PHAsset
@@ -54,11 +58,10 @@ import SystemConfiguration
 //              print(pivPHAsset as PHAsset)
 //              print(PHAssetResource.assetResources(for: pivPHAsset).first?.uniformTypeIdentifier)
               
-              
-                   
                pivPHAsset.requestContentEditingInput(with: PHContentEditingInputRequestOptions()) { (input, _) in
                    let fileURL = input!.fullSizeImageURL
 //                   let dataImage: NSData = NSData(contentsOfFile: fileURL!.path)!
+                   print(fileURL)
 
                    let headers: HTTPHeaders = [
                      "content-type": "multipart/form-data",
