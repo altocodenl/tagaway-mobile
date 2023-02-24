c61548fee6c639207b4b22010cb3706d56fffc08
tomsawada <tomydrum@gmail.com>
2023-02-24 10:41:50 -0400

    commented photo manager in uploaded view

--- tagaway/lib/views/uploadedView.dart
+++ tagaway/lib/views/uploadedView.dart
@@ -189,33 +189,33 @@ class _UploadGridState extends State<UploadGrid> {
   loadList() {
     itemList = [];
     selectedList = [];
-    _fetchAssets();
+    // _fetchAssets();
   }
 
-  _fetchAssets() async {
-    FilterOptionGroup makeOption() {
-      // final option = FilterOption();
-      return FilterOptionGroup()
-        ..addOrderOption(
-            const OrderOption(type: OrderOptionType.createDate, asc: false));
-    }
-
-    final option = makeOption();
-    // Set onlyAll to true, to fetch only the 'Recent' album
-    // which contains all the photos/videos in the storage
-    final albums = await PhotoManager.getAssetPathList(
-        onlyAll: true, filterOption: option);
-    final recentAlbum = albums.first;
-
-    // Now that we got the album, fetch all the assets it contains
-    final recentAssets = await recentAlbum.getAssetListRange(
-      start: 0, // start at index 0
-      end: 1000000, // end at a very big index (to get all the assets)
-    );
-
-    // Update the state and notify UI
-    setState(() => itemList = recentAssets);
-  }
+  // _fetchAssets() async {
+  //   FilterOptionGroup makeOption() {
+  //     // final option = FilterOption();
+  //     return FilterOptionGroup()
+  //       ..addOrderOption(
+  //           const OrderOption(type: OrderOptionType.createDate, asc: false));
+  //   }
+  //
+  //   final option = makeOption();
+  //   // Set onlyAll to true, to fetch only the 'Recent' album
+  //   // which contains all the photos/videos in the storage
+  //   final albums = await PhotoManager.getAssetPathList(
+  //       onlyAll: true, filterOption: option);
+  //   final recentAlbum = albums.first;
+  //
+  //   // Now that we got the album, fetch all the assets it contains
+  //   final recentAssets = await recentAlbum.getAssetListRange(
+  //     start: 0, // start at index 0
+  //     end: 1000000, // end at a very big index (to get all the assets)
+  //   );
+  //
+  //   // Update the state and notify UI
+  //   setState(() => itemList = recentAssets);
+  // }
 
   @override
   Widget build(BuildContext context) {
