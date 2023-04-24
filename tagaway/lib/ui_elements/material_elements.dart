6b55ba2e1ec671b9e05646ab183ce68d36a9f949
Federico Pereiro <fpereiro@gmail.com>
2023-04-24 22:11:48 +0200

    Fix range header in Android

--- tagaway/lib/ui_elements/material_elements.dart
+++ tagaway/lib/ui_elements/material_elements.dart
@@ -629,7 +629,7 @@ class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
         'cookie': StoreService.instance.get('cookie'),
         // This line turns off cache. Cache in video_player seems to be broken.
         'if-none-match': 'foo',
-        'Range': 'bytes=0',
+        'Range': 'bytes=0-',
       },
     );
     // Play the video again when it ends
