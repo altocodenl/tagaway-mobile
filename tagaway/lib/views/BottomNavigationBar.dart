fbe3bc125af427a61d3c281fdb77e6c046a47389
Federico Pereiro <fpereiro@gmail.com>
2023-12-04 22:04:50 +0100

    Merge

--- tagaway/lib/views/BottomNavigationBar.dart
+++ tagaway/lib/views/BottomNavigationBar.dart
@@ -1,6 +1,7 @@
 // IMPORT FLUTTER PACKAGES
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tagaway/services/sizeService.dart';
 import 'package:tagaway/services/storeService.dart';
 
 // IMPORT UI ELEMENTS
