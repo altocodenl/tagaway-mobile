5f7329f2301e942472d1675ab26f94726b511d5b
Federico Pereiro <fpereiro@gmail.com>
2024-01-18 21:33:19 +0100

    Fix size of icon

--- tagaway/lib/views/BottomNavigationBar.dart
+++ tagaway/lib/views/BottomNavigationBar.dart
@@ -134,7 +134,7 @@ class _UploadingNumberState extends State<UploadingNumber> {
               const Icon(
                 kArrowLeftLong,
                 color: kAltoBlue,
-                size: 15,
+                size: 13,
               ),
             ],
           ),
