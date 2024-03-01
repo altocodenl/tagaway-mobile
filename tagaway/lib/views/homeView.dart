8722e5ab56ef736235270d86a6a636a3943d8848
Federico Pereiro <fpereiro@gmail.com>
2024-03-01 22:15:40 +0100

    Dynamizing local pivs

--- tagaway/lib/views/homeView.dart
+++ tagaway/lib/views/homeView.dart
@@ -1,6 +1,12 @@
+import 'dart:io' show File;
 import 'package:flutter/material.dart';
+import 'package:photo_manager/photo_manager.dart';
+import 'package:video_player/video_player.dart';
 
-import '../ui_elements/constants.dart';
+import 'package:tagaway/services/sizeService.dart';
+import 'package:tagaway/services/tagService.dart';
+import 'package:tagaway/services/tools.dart';
+import 'package:tagaway/ui_elements/constants.dart';
 
 class HomeView extends StatefulWidget {
   static const String id = 'home';
@@ -11,6 +17,27 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  dynamic cancelListener;
+
+  dynamic queryResult = {'pivs': [], 'total': 0};
+
+  @override
+  void initState() {
+    super.initState();
+    TagService.instance.queryPivs();
+    cancelListener = store.listen(['queryResult'], (QueryResult) {
+      setState(() {
+        if (QueryResult != '') queryResult = QueryResult;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    cancelListener();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,20 +60,181 @@ class _HomeViewState extends State<HomeView> {
         child: CustomScrollView(
           slivers: [
             SliverList.builder(
-                itemCount: 10,
+                itemCount: queryResult['pivs'].length,
                 itemBuilder: (BuildContext context, int index) {
                   return Padding(
-                    padding: const EdgeInsets.only(bottom: 40),
-                    child: Container(
-                      height: 100,
-                      alignment: Alignment.center,
-                      color: Colors.lightBlue,
-                    ),
-                  );
+                      padding: const EdgeInsets.only(bottom: 40),
+                      child: (() {
+                        var piv = queryResult['pivs'][index];
+                        if (piv['local'] == true &&
+                            piv['piv'].type == AssetType.image)
+                          return LocalPhoto(piv: piv['piv']);
+                        if (piv['local'] == true &&
+                            piv['piv'].type != AssetType.image)
+                          return LocalVideo(vid: piv['piv']);
+                        return Container(
+                          height: 100,
+                          alignment: Alignment.center,
+                          color: Colors.lightBlue,
+                        );
+                      })());
                 })
           ],
         ),
       ),
     );
+  }
+}
+
+class LocalPhoto extends StatefulWidget {
+  final dynamic piv;
+  const LocalPhoto({Key? key, required this.piv}) : super(key: key);
+
+  @override
+  State<LocalPhoto> createState() => _LocalPhotoState();
+}
+
+class _LocalPhotoState extends State<LocalPhoto> {
+  dynamic cancelListener;
+
+  Future<File?> loadImage(piv) async {
+    var file = await piv.file;
+    return file;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    cancelListener = store.listen(['foo'], (Foo) {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    cancelListener();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Future<File?> file = loadImage(widget.piv);
+    return Container(
+      alignment: Alignment.center,
+      child: FutureBuilder<File?>(
+        future: file,
+        builder: (_, snapshot) {
+          final file = snapshot.data;
+          if (file == null) return Container();
+          return Image.file(file);
+        },
+      ),
+    );
+  }
+}
+
+class LocalVideo extends StatefulWidget {
+  const LocalVideo({Key? key, required this.vid}) : super(key: key);
+  final AssetEntity vid;
+
+  @override
+  _LocalVideoState createState() => _LocalVideoState();
+}
+
+class _LocalVideoState extends State<LocalVideo> {
+  late VideoPlayerController _controller;
+  bool initialized = false;
+  bool fullScreen = false;
+  dynamic cancelListener;
+
+  @override
+  void initState() {
+    _initVideo();
+    setState(() {
+      fullScreen = store.get('fullScreenCarrousel') == true;
+    });
+    cancelListener =
+        store.listen(['fullScreenCarrousel'], (FullScreenCarrousel) {
+      setState(() {
+        fullScreen = FullScreenCarrousel == true;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    cancelListener();
+    super.dispose();
+  }
+
+  _initVideo() async {
+    final video = await widget.vid.file;
+    _controller = VideoPlayerController.file(video!)
+      // Play the video again when it ends
+      ..setLooping(true)
+      // initialize the controller and notify UI when done
+      ..initialize().then((_) => setState(() {
+            initialized = true;
+            _controller.play();
+          }));
+    // _controller.play();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return initialized
+        // If the video is initialized, display it
+        ? Stack(children: [
+            AnimatedContainer(
+              width: SizeService.instance.screenWidth(context),
+              height: fullScreen
+                  ? SizeService.instance.screenHeight(context) * .85
+                  : SizeService.instance.screenHeight(context) * .45,
+              duration: const Duration(milliseconds: 500),
+              alignment: Alignment.topCenter,
+              decoration: const BoxDecoration(
+                  color: kGreyDarker,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15))),
+              child: AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                // Use the VideoPlayer widget to display the video.
+                child: VideoPlayer(_controller),
+              ),
+            ),
+            Align(
+              alignment: fullScreen ? Alignment(0, .83) : Alignment(0, 0),
+              child: FloatingActionButton(
+                shape: const CircleBorder(),
+                backgroundColor: kAltoBlue,
+                onPressed: () {
+                  // Wrap the play or pause in a call to `setState`. This ensures the
+                  // correct icon is shown.
+                  setState(() {
+                    // If the video is playing, pause it.
+                    if (_controller.value.isPlaying) {
+                      _controller.pause();
+                    } else {
+                      // If the video is paused, play it.
+                      _controller.play();
+                    }
+                  });
+                },
+                // Display the correct icon depending on the state of the player.
+                child: Icon(
+                  _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ])
+        // If the video is not yet initialized, display a spinner
+        : const Center(
+            child: CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(kAltoBlue),
+          ));
   }
 }
