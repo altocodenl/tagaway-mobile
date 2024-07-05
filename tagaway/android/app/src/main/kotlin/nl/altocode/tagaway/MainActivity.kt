30033aed7600e5e3eefd0a08888fbc8c58fc8a8d
Federico Pereiro <fpereiro@gmail.com>
2024-07-05 21:59:07 +0200

    Fixing the unfixable

--- tagaway/android/app/src/main/kotlin/nl/altocode/tagaway/MainActivity.kt
+++ tagaway/android/app/src/main/kotlin/nl/altocode/tagaway/MainActivity.kt
@@ -1,6 +1,9 @@
 package nl.altocode.tagaway
-
+import android.os.Bundle
+import android.os.StatFs
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
