c7f7eafb16cb74b56635c923706b9d87e1a0c31e
Federico Pereiro <fpereiro@gmail.com>
2024-07-05 21:46:25 +0200

    Re-add method channel for space

--- tagaway/android/app/src/main/kotlin/nl/altocode/tagaway/MainActivity.kt
+++ tagaway/android/app/src/main/kotlin/nl/altocode/tagaway/MainActivity.kt
@@ -3,4 +3,21 @@ package nl.altocode.tagaway
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+    private val CHANNEL = "nl.tagaway/storage"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "getAvailableStorage") {
+                val stat = StatFs("/data")
+                val availableBytes = stat.availableBytes
+                result.success(availableBytes)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
 }
